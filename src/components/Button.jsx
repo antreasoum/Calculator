32db418d8f3e042f8cd86fdb710ea92c7b1b1bd6
antreasoum <antreasoum@gmail.com>
2020-03-31 16:36:37 +0300

    Not Functional

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css';
+
+export const isOperator = val => {
+    return !isNaN(val) || val === "." || val === "=";
+}
+
+const Button = props => (
+    <div className={`button-wrapper ${
+        isOperator(props.children) ? null: "Operator" }`} >{props.children}
+    </div>
+)
+
---
+++ src/components/Input.jsx
@@ -0,0 +1,4 @@
+import react from 'react';
+import './Input.css';
+
+export const
