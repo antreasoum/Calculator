a5d3cd82149d72b2916141a5aa1fda38467fc4f4
antreasoum <antreasoum@gmail.com>
2020-03-31 18:08:00 +0300

    Not Functional 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import {Button} from './components/Button';
 import {Input} from './components/Input';
+import {ClearButton} from './components/ClearButton';
 
 
 class App extends Component {
@@ -18,8 +19,10 @@ class App extends Component {
     <div className="app">
       <div className="calc-wrapper">
 
+            {/*INPUT*/}
         <Input input={this.state.input}></Input>
 
+            {/*ROWS*/}
         <div className="row">
           <Button>7</Button>
           <Button>8</Button>
@@ -47,6 +50,10 @@ class App extends Component {
           <Button>=</Button>
           <Button>-</Button>
         </div>
+
+        <div className="row">
+          <ClearButton handleClear={() => this.setState({input: ""})}> Clear </ClearButton>
+      </div>
 
       </div>
     </div>);
